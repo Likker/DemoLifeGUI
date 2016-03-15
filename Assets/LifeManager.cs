2854e0f5ff1f878feee1126a4d37732f80d16d11
Paulo <paulcmoi51@gmail.com>
2016-03-16 06:53:03 +0800

    demo

---
+++ Assets/LifeManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour {
+
+	private Slider lifeSlider;
+
+	// Use this for initialization
+	void Start () {
+		lifeSlider = this.transform.GetChild (0).GetComponentInChildren<Slider> ();
+		Debug.Log (lifeSlider.value);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void addLife()
+	{
+		lifeSlider.value += 0.2f;
+	}
+
+	public void removeLife()
+	{
+		lifeSlider.value -= 0.2f;
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+	public float velocity = 1;
+	private float tempTime;
+
+	// Use this.gameObject for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+				this.gameObject.gameObject.transform.localPosition = new Vector3 (this.gameObject.transform.localPosition.x - Time.deltaTime * velocity,
+				this.gameObject.transform.localPosition.y,
+				this.gameObject.transform.localPosition.z);	}
+
+		if (Input.GetKey (KeyCode.RightArrow))
+		{
+			this.gameObject.transform.localPosition = new Vector3 (this.gameObject.transform.localPosition.x + Time.deltaTime * velocity,
+				this.gameObject.transform.localPosition.y,
+				this.gameObject.transform.localPosition.z);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) 
+		{
+			processJump ();
+		}
+
+	}
+
+	void processJump()
+	{
+		//GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+	}
+
+
+}
